4df237e53863aa9f09b36c3261c1d884f459b0a5
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2022-05-15 16:29:16 +0200

    Added a Jupyter notebook for Boston linear regression

--- boston_lin_regr.py
+++ boston_lin_regr.py
@@ -1,3 +1,4 @@
+# %%
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -10,64 +11,15 @@ import statsmodels.api as sm
 from sklearn.metrics import mean_squared_error
 
 plt.ioff()
-
-"""
-This is an exploration of linear regression, and various tools available to solve it, with Python
-The dataset of choice is the Boston Housing Prices http://lib.stat.cmu.edu/datasets/boston
-"""
-
-# Fetch the dataset from its original source
-# run block of code and catch warnings
-with warnings.catch_warnings():
-    # ignore all caught warnings
-    warnings.filterwarnings("ignore")
-    boston = datasets.load_boston()
-
-dataset_X = DataFrame(boston.data, columns=boston.feature_names)
-dataset_X.drop(['B'], axis=1, inplace=True)
-dataset_y = Series(boston.target)
-dataset = dataset_X.copy()
-dataset['MEDV'] = dataset_y
-n_samples = len(dataset)
-n_vars = len(dataset.columns) - 1
-print(f'\nThe dataset contains {n_samples} samples, and {n_vars} variables (target not included).')
-
 pd.set_option('display.max_columns', None)  # Print all the columns in the dataframe
 pd.set_option('display.expand_frame_repr', False)  # Prevents line breaks while printing the dataframe
 
-print('\nThe first 5 samples from the dataset')
-print(dataset.head())
-
-print('\nDataset stats')
-print(dataset.describe().T)
-
-print('\nCount of unique values for each variable')
-counts = dataset.nunique()
-print(counts.to_frame().T)
-
-print('\nCount number of NaN for each variable')
-print(dataset.isna().sum().to_frame().T)
-
-# Draw box plots for every variable, one under another (each has its own scale)
-# fig, axes = plt.subplots(len(dataset.columns), 1)
-fig, axes = plt.subplots(1, n_vars + 1)
-fig.subplots_adjust(wspace=.5)
-for i, col in enumerate(dataset.columns):
-    bplot = sns.boxplot(y=dataset[col], ax=axes[i])
-    bplot.set(ylabel=None)
-    bplot.set(xlabel=col)
-# plt.pause(0)
-
+# %% [md]
 """
-sns.pairplot(dataset_X[['CRIM','ZN','INDUS']], diag_kind='kde')
-sns.pairplot(dataset_X, diag_kind='kde')
-plt.show()
-plt.pause(0)
-input('Press [Enter]')
-"""
-'''
+This is an exploration of linear regression, and different tools available to solve it with Python.
+The dataset of choice is the Boston Housing Prices http://lib.stat.cmu.edu/datasets/boston
+
 Here for reference the meaning of the variables, taken from the dataset description https://bit.ly/3yqTZnL
-
     CRIM - per capita crime rate by town
     ZN - proportion of residential land zoned for lots over 25,000 sq.ft.
     INDUS - proportion of non-retail business acres per town.
@@ -82,18 +34,74 @@ Here for reference the meaning of the variables, taken from the dataset descript
     B - 1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town
     LSTAT - % lower status of the population
     MEDV - Median value of owner-occupied homes in $1000's
-    
-We start with a simple (that is, uni-variate) linear regression of MEDV against LSTAT. 
-'''
+"""
 
-'''
-Distribution of LSTAT values
-'''
+# %% [md]
+# Fetch the dataset from its original source and load it
 
+# %%
+with warnings.catch_warnings():
+    # ignore all caught warnings
+    warnings.filterwarnings("ignore")
+    boston = datasets.load_boston()
+
+"""
+Dropping the 'B' variable from the dataset: it is a non-invertible variable engineered by the authors of the dataset;
+it's not clear how to use it for analysis to follow, whatever the usage intended by the dataset authors.
+"""
+columns = boston.feature_names[boston.feature_names != 'B']
+
+"""
+dataset_X => predictors only
+dataset_y => target variable
+dataset => predictors and target variable
+"""
+dataset_X = DataFrame(boston.data, columns=boston.feature_names)
+dataset_y = Series(boston.target)
+dataset = dataset_X.copy()
+dataset['MEDV'] = dataset_y  # Assign the correct name to the target variable
+
+n_samples = len(dataset)
+n_vars = len(dataset.columns) - 1
+print(f'\nThe dataset contains {n_samples} samples, and {n_vars} variables (target not included).')
+
+print('\nThe first 5 samples from the dataset')
+print(dataset.head())
+
+print('\nDataset stats')
+print(dataset.describe().T)
+
+print('\nCount of unique values for each variable')
+counts = dataset.nunique()
+print(counts.to_frame().T)
+
+print('\nCount number of NaN for each variable')
+print(dataset.isna().sum().to_frame().T)
+
+# %% [md]
+# Draw box plots for every variable, side by side (each has its own scale)
+
+# %%
+fig, axes = plt.subplots(1, n_vars + 1)
+fig.subplots_adjust(wspace=.5)
+for i, col in enumerate(dataset.columns):
+    bplot = sns.boxplot(y=dataset[col], ax=axes[i])
+    bplot.set(ylabel=None)
+    bplot.set(xlabel=col)
+
+# %% [md]
+# We start with a simple (that is, uni-variate) linear regression of MEDV against LSTAT.
+
+# %% [md]
+# Plotting the distribution of LSTAT values
+
+# %%
 plt.hist(dataset['LSTAT'], bins=50)
-# plt.pause(0)
 
-# Performa the simple linear regression of MEDV against LSTAT
+# %% [md]
+# Performa the simple linear regression of MEDV against LSTAT and output its stats
+
+# %%
 X, y = dataset['LSTAT'].values.reshape(-1, 1), dataset['MEDV'].values
 model: LinearRegression = LinearRegression().fit(X, y)
 y_pred = model.predict(X=X)
@@ -105,6 +113,10 @@ rss = np.sum(np.power(y - y_pred, 2))
 rse = (rss / (n_samples - 2)) ** .5
 print(f'RSS={rss:.3f}; RSE={rse:.3f} on {n_samples - 2} degrees of freedom; R-squared={r2:.3f}')
 
+# %% [md]
+# Plot the linear regression against the data
+
+# %%
 _, ax = plt.subplots()
 ax.scatter(X, y, facecolor='none', edgecolor='blue')
 ax.plot(X, y_pred, color='black')
@@ -113,12 +125,14 @@ ax.set_ylabel('MEDV')
 ax.set_title('Linear Regression of MEDV over LSTAT')
 # plt.pause(0)
 
+# %% [md]
 ''' 
 IS there a relationship between MEDV and LSTAT? The linear regression plot indicates so, in spite of outliers toward 
 high MEDV values; however, more formally, we want to test the null hypothesis
     -There is no relationship between MEDV and LSTAT
 '''
 
+# %%
 ''' We can get a feeling of how accurate the estimate of slope and intercept are, by determining a confidence interval 
 for them '''
 X_mean = np.mean(X)
@@ -139,10 +153,11 @@ _, ax = plt.subplots()
 ax.plot(x, y_est, '-')
 ax.fill_between(x, y_est - y_err, y_est + y_err, alpha=0.2)
 ax.plot(X, y, 'o', color='tab:brown')
-# plt.pause(0)
 
+# %% [md]
 # Let's do it again, this time with Statsmodel
 
+# %%
 X_exog = sm.add_constant(X, prepend=False)  # TODO this is needed for correct result, but why?
 model2 = sm.OLS(y, X_exog)
 res = model2.fit()
@@ -169,11 +184,14 @@ inference; see y_err above
 Note: se2_intercept and se2_slope appear to be correct when compared to R output
 '''
 
+# %% [md]
 # Now let's do some multi-variate regression
 
+# %%
 X, y = dataset_X, dataset_y
 mv_model: LinearRegression = LinearRegression().fit(X, y)
 coeffs = pd.Series(mv_model.coef_, index=mv_model.feature_names_in_)
+print(f'\nCoefficients of multi-variate linear regression, with intercept {mv_model.intercept_:.6f}')
 print(coeffs.to_frame().T)
 
 y_pred = mv_model.predict(X)
@@ -183,10 +201,14 @@ r2 = mv_model.score(X, y)
 rss = np.sum(np.power(y - y_pred, 2))
 rse = (rss / (n_samples - 2)) ** .5
 print(f'RSS={rss:.3f}; RSE={rse:.3f} on {n_samples - 2} degrees of freedom; R-squared={r2:.3f}')
+print('\nThe correlation matrix')
 corr_matrix = dataset.corr()
 print(corr_matrix)
 
+# %% [md]
 # Display the correlation matrix as a heatmap with Seaborn
+
+# %%
 plt.subplots()
 corr_matrix_rounded = corr_matrix.round(decimals=1)
 ax = sns.heatmap(corr_matrix_rounded,
@@ -200,4 +222,13 @@ ax.set_xticklabels(ax.get_xticklabels(),
                    rotation=45,
                    horizontalalignment='right')
 # fig.tight_layout()
-plt.pause(0)
+# plt.pause(0)
+
+# %% [md]
+# Let's give it a spin with Statsmodel
+# %%
+print('\nSummary of multi-variate linear regression with Statsmodel')
+X_exog = sm.add_constant(dataset_X, prepend=False)  # TODO this is needed for correct result, but why?
+model2 = sm.OLS(dataset_y, X_exog)
+res = model2.fit()
+print('\n', res.summary())
