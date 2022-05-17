17829d6b983622b3b687f5540f17d79cfa6c591e
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2022-05-17 16:12:20 +0200

    Split some utility code into its own source file

---
+++ test_utils.py
@@ -0,0 +1,19 @@
+from sklearn.linear_model import LinearRegression
+import utils
+
+
+def test_load_boston_housing_market():
+    dataset_X, dataset_y, dataset = utils.load_boston_housing_market()
+    assert dataset_X.shape == (506, 12)
+    assert dataset_y.shape == (506,)
+    assert dataset.shape == (506, 13)
+    assert list(dataset.columns) == ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX',
+                                     'PTRATIO', 'LSTAT', 'MEDV']
+
+
+def test_forward_select():
+    X, y, _ = utils.load_boston_housing_market()
+    selected, r2 = utils.forward_select(LinearRegression, X, y)
+    assert selected == ['LSTAT', 'RM', 'PTRATIO', 'DIS', 'NOX', 'CHAS', 'ZN', 'CRIM', 'RAD', 'TAX', 'AGE', 'INDUS']
+    assert r2[0] == .5441462975864797
+    assert r2[-1] == .7343070437613076
---
+++ utils.py
@@ -0,0 +1,52 @@
+import warnings
+from sklearn import datasets
+from pandas import DataFrame, Series
+
+
+def load_boston_housing_market():
+    with warnings.catch_warnings():
+        # ignore all caught warnings
+        warnings.filterwarnings("ignore")
+        boston = datasets.load_boston()
+
+    """
+    dataset_X => predictors
+    dataset_y => target variable
+    dataset => predictors and target variable
+    """
+    dataset_X = DataFrame(boston.data, columns=boston.feature_names)
+    """
+    Dropping the 'B' variable from the dataset: it is a non-invertible variable engineered by the authors of the dataset; it is not clear how to use it for the analysis to follow, whatever the usage intended by the dataset authors.
+    """
+    dataset_X.drop('B', axis=1, inplace=True)
+    dataset_y = Series(boston.target)
+    dataset = dataset_X.copy()
+    dataset['MEDV'] = dataset_y  # Assign the correct name to the target variable
+    return dataset_X, dataset_y, dataset
+
+
+def forward_select(model, X, y):
+    selected = []
+    candidates = list(X.columns)
+    best_r2 = -1
+    all_r2 = []
+    while candidates:
+        best_candidate = None
+        for candidate in candidates:
+            X_selected = X[selected + [candidate]].copy()
+            res = model().fit(X_selected, y)
+            r2 = res.score(X_selected, y)
+            if r2 > best_r2:
+                best_candidate = candidate
+                best_r2 = r2
+        if best_candidate is None:
+            break
+        else:
+            selected.append(best_candidate)
+            candidates = [item for item in candidates if item != best_candidate]
+            all_r2.append(best_r2)
+    return selected, all_r2
+
+
+if __name__ == '__main__':
+    ...
